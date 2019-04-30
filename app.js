43e92b74f892f755201079e2cfc4eb46827e76ad
ReinVentxHerman <herman5b22@yahoo.com.hk>
2019-04-30 15:12:04 +0800

    herman
    change url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost');
+    res.header('Access-Control-Allow-Origin', ' *');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept');
     next();
